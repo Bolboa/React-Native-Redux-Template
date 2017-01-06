3e2eb06399fd664a10a7013755ade9ac5d4a44d4
Eric Bolboa <13eb10@queensu.ca>
2017-01-06 00:48:15 -0500

    React Hello World

---
+++ containers/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Navigator
+} from 'react-native';
+
+export default Home = () => {
+	return (
+        <View>
+        	<Text>Hello World</Text>
+        </View>
+	);
+}
---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Home from './containers/index';
+import {AppRegistry} from 'react-native';
+
+AppRegistry.registerComponent('Template', () => Home);
